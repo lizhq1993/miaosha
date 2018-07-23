97be57761908238e3d09b2704cb8d1e8bc1df64a
lzq4 <lzq4@meitu.com>
2018-07-23 19:53:22 +0800

    add isMoble validation

--- src/main/java/com/lzhq/miaosha/vo/LoginVo.java
+++ src/main/java/com/lzhq/miaosha/vo/LoginVo.java
@@ -1,5 +1,6 @@
 package com.lzhq.miaosha.vo;
 
+import com.lzhq.miaosha.validator.IsMobile;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotNull;
 public class LoginVo {
 
     @NotNull
+    @IsMobile
     private String mobile;
 
     @NotNull
